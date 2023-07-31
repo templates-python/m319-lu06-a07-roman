b3bebe6a26123d8e227c8b54274fe1d12aa39eae
Marcel Suter <marcel.suter@bzz.ch>
2023-07-31 11:02:46 +0200

    initial commit

--- roman.py
+++ roman.py
@@ -1,6 +1,18 @@
 def main():
-    # write the program logic here and remove the line "pass"
-    pass
+    numerals = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
+    decimals = [1000, 500, 100, 50, 10, 5, 1]
+    count = 0
+    result = ''
+
+    number = int(input('Dezimalzahl > '))
+    while number > 0:
+        if (number >= decimals[count]):
+            result += numerals[count]
+            number = number - decimals[count]
+        else:
+            count = count + 1
+    print(result)
+    return numerals, decimals
 
 
 if __name__ == '__main__':
